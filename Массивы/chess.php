4750869268a7d766a9ccf9e481bf49e5db4f5902
User <funikov.1997@mail.ru>
2021-12-19 21:38:04 +0300

    Вывод ладьи в доску

--- Массивы/chess.php
+++ Массивы/chess.php
@@ -1,6 +1,11 @@
 <?php
 
-echo '<table border="1">';
+echo '<table border="1" style="margin: auto">';
+$sym = ['A','B','C','D','E','F','G','H'];
+
+$rand_i = rand(1,8);
+$rand_k = rand(1,8);
+
 for ($i = 1; $i <= 8; $i++) {
     echo '<tr>';
 
@@ -13,6 +18,7 @@ for ($i = 1; $i <= 8; $i++) {
     }
 
     for ($k = 1; $k <= 8; $k++) {
+        $coordinates_row[] = $i.'_'.$k;
         // Если строка чётная
         if ($line == 'even') {
             if ($k % 2 != 0 ) {
@@ -30,8 +36,18 @@ for ($i = 1; $i <= 8; $i++) {
                 $color = 'white';
             }
         }
-        echo "<td style='background-color: $color; width: 80px; height: 80px;'></td>";
+        if ($i == $rand_i AND $k == $rand_k) {
+            $ladya_photo = '<img style="width: 20px; height: 50px;margin: 0px auto 0px; display: block;" src="ladya.png">';
+        }
+        else {
+            $ladya_photo = '';
+        }
+        echo "<td style='background-color: $color; width: 80px; height: 80px;'>$ladya_photo</td>";
     }
     echo '</tr>';
 }
 echo '</table>';
+
+echo '<pre>';
+var_dump($coordinates_row);
+echo '</pre>';
