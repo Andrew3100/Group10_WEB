3d0a6fb51d3c25cda1f0fbd9f5e34121c974346a
Andrey <funikov.1997@mail.ru>
2021-12-21 00:11:34 +0300

    Альтернативное решение задачи на шахматы

--- Массивы/chess.php
+++ Массивы/chess.php
@@ -1,5 +1,68 @@
 <?php
 
+
+
+
+$rand_ladya_x = rand(1,8).'_'.rand(1,8);
+
+
+$rand_peshka_1 = $rand_ladya_x;
+$rand_peshka_2 = $rand_ladya_x;
+$rand_peshka_3 = $rand_ladya_x;
+
+// Цикл проверяет, чтобы пешки не наложились на ладью
+while ($rand_peshka_1 == $rand_ladya_x OR $rand_peshka_2 == $rand_ladya_x OR $rand_peshka_3 == $rand_ladya_x) {
+    $rand_peshka_1 = rand(1,8).'_'.rand(1,8);
+    $rand_peshka_2 = rand(1,8).'_'.rand(1,8);
+    $rand_peshka_3 = rand(1,8).'_'.rand(1,8);
+}
+
+
+$j = 0;
+for ($i = 1; $i <= 8; $i++) {
+    for ($k = 1; $k <= 8; $k++) {
+
+
+
+
+        // Соединяем текущие координаты, чтобы сравнить их со случайными координатами клеток
+        $fix_figure = $k.'_'.$i;
+        // Если соединённые координаты равны координатам либо первой, либо второй, либо третьей пешки
+        if ($fix_figure == $rand_peshka_1 OR $fix_figure == $rand_peshka_2 OR $fix_figure == $rand_peshka_3) {
+            // то в $figure пишем наличие фигуры
+            $figure = 'Тут расположена пешка';
+        }
+        else {
+            // Если нет, то пустое значение
+            $figure = NULL;
+        }
+
+        // Ключ - координата клетки. Значение - массив, в котором элементы
+        // $attack - клетка под атакой ладьи или нет
+        // $figure - если 1, то в этой клетке стоит пешка, 0 - клетка пустая
+        // $color  - цвет клетки
+        $data[$i.'_'.$k] = [
+                $figure
+        ];
+    }
+}
+
+echo '<pre>';
+var_dump($data);
+echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+exit();
+
 echo '<table border="1" style="margin: auto">';
 $sym = ['A','B','C','D','E','F','G','H'];
 
