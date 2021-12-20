f1ad761176fde8a9813219ab4a69649701c3eb1c
Andrey <funikov.1997@mail.ru>
2021-12-21 00:31:36 +0300

    Альтернативное решение задачи на шахматы

--- Массивы/chess.php
+++ Массивы/chess.php
@@ -3,7 +3,9 @@
 
 
 
-$rand_ladya_x = rand(1,8).'_'.rand(1,8);
+$rand_ladya_x = rand(1,8);
+$rand_ladya_y = rand(1,8);
+$rand_ladya_coord = $rand_ladya_x.'_'.$rand_ladya_y;
 
 
 $rand_peshka_1 = $rand_ladya_x;
@@ -11,7 +13,7 @@ $rand_peshka_2 = $rand_ladya_x;
 $rand_peshka_3 = $rand_ladya_x;
 
 // Цикл проверяет, чтобы пешки не наложились на ладью
-while ($rand_peshka_1 == $rand_ladya_x OR $rand_peshka_2 == $rand_ladya_x OR $rand_peshka_3 == $rand_ladya_x) {
+while ($rand_peshka_1 == $rand_ladya_coord OR $rand_peshka_2 == $rand_ladya_coord OR $rand_peshka_3 == $rand_ladya_coord) {
     $rand_peshka_1 = rand(1,8).'_'.rand(1,8);
     $rand_peshka_2 = rand(1,8).'_'.rand(1,8);
     $rand_peshka_3 = rand(1,8).'_'.rand(1,8);
@@ -20,9 +22,20 @@ while ($rand_peshka_1 == $rand_ladya_x OR $rand_peshka_2 == $rand_ladya_x OR $ra
 
 $j = 0;
 for ($i = 1; $i <= 8; $i++) {
+    if ($i == $rand_ladya_x) {
+        $attack = 1;
+    }
+    else {
+        $attack = 0;
+    }
     for ($k = 1; $k <= 8; $k++) {
 
-
+        if ($i == $rand_ladya_y) {
+            $attack1 = 1;
+        }
+        else {
+            $attack1 = 0;
+        }
 
 
         // Соединяем текущие координаты, чтобы сравнить их со случайными координатами клеток
@@ -41,37 +54,49 @@ for ($i = 1; $i <= 8; $i++) {
         // $attack - клетка под атакой ладьи или нет
         // $figure - если 1, то в этой клетке стоит пешка, 0 - клетка пустая
         // $color  - цвет клетки
+        $attack3 = $attack.'_'.$attack1;
         $data[$i.'_'.$k] = [
-                $figure
+                $figure,
+                $attack3,
         ];
     }
 }
-
 echo '<pre>';
 var_dump($data);
 echo '</pre>';
-
-
-
-
-
-
-
-
-
-
-
 exit();
+
+
+//echo '<pre>';
+//var_dump($data);
+//echo '</pre>';
+//
+//
+//
+//
+
+
+
+
+
+
+
+
 
 echo '<table border="1" style="margin: auto">';
 $sym = ['A','B','C','D','E','F','G','H'];
 
-$rand_i = rand(1,8);
-$rand_k = rand(1,8);
+$rand_i = 2/*rand(1,8)*/;
+$rand_k = 2/*rand(1,8)*/;
 
 for ($i = 1; $i <= 8; $i++) {
     echo '<tr>';
-
+    if ($i == $rand_i) {
+        $attack = 1;
+    }
+    else {
+        $attack = 0;
+    }
     if ($i%2==0) {
         // Фиксируем в переменной тот факт, что обрабатывается чётная строка
         $line = 'even';
@@ -81,6 +106,12 @@ for ($i = 1; $i <= 8; $i++) {
     }
 
     for ($k = 1; $k <= 8; $k++) {
+        if ($k == $rand_k) {
+            $attack1 = 1;
+        }
+        else {
+            $attack1 = 0;
+        }
         $coordinates_row[] = $i.'_'.$k;
         // Если строка чётная
         if ($line == 'even') {
@@ -105,7 +136,9 @@ for ($i = 1; $i <= 8; $i++) {
         else {
             $ladya_photo = '';
         }
-        echo "<td style='background-color: $color; width: 80px; height: 80px;'>$ladya_photo</td>";
+        $attack3 = $attack.'_'.$attack1;
+        echo "<td style='background-color: $color; width: 80px; height: 80px;'>$ladya_photo $attack3</td>";
+
     }
     echo '</tr>';
 }
