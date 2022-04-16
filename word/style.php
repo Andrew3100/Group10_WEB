b7b3396a125260ebc428522b00c291e48f8384e0
User <funikov.1997@mail.ru>
2022-04-16 12:50:49 +0300

    Работа с документом ворд, печать браузер

---
+++ word/style.php
@@ -0,0 +1,123 @@
+<?php
+
+$style = "
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:'Cambria Math';
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:Cambria;
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Tahoma;
+	panose-1:2 11 6 4 3 5 4 4 2 4;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:3.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	font-size:12.0pt;
+	font-family:'Times New Roman',serif;}
+h1
+	{mso-style-link:'Заголовок 1 Знак';
+	margin-right:0cm;
+	margin-left:0cm;
+	font-size:24.0pt;
+	font-family:'Times New Roman',serif;}
+h2
+	{mso-style-link:'Заголовок 2 Знак';
+	margin-top:12.0pt;
+	margin-right:0cm;
+	margin-bottom:3.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	page-break-after:avoid;
+	font-size:14.0pt;
+	font-family:'Arial',sans-serif;
+	font-style:italic;}
+p.MsoCommentText, li.MsoCommentText, div.MsoCommentText
+	{mso-style-link:'Текст примечания Знак';
+	margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:3.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	font-size:10.0pt;
+	font-family:'Times New Roman',serif;}
+span.MsoCommentReference
+	{font-family:'Times New Roman',serif;}
+p.MsoCommentSubject, li.MsoCommentSubject, div.MsoCommentSubject
+	{mso-style-link:'Тема примечания Знак';
+	margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:3.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	font-size:10.0pt;
+	font-family:'Times New Roman',serif;
+	font-weight:bold;}
+p.MsoAcetate, li.MsoAcetate, div.MsoAcetate
+	{mso-style-link:'Текст выноски Знак';
+	margin:0cm;
+	margin-bottom:.0001pt;
+	text-align:justify;
+	font-size:8.0pt;
+	font-family:'Tahoma',sans-serif;}
+span.1
+	{mso-style-name:'Заголовок 1 Знак';
+	mso-style-link:'Заголовок 1';
+	font-family:'Cambria',serif;
+	font-weight:bold;}
+span.2
+	{mso-style-name:'Заголовок 2 Знак';
+	mso-style-link:'Заголовок 2';
+	font-family:'Cambria',serif;
+	font-weight:bold;
+	font-style:italic;}
+span.a
+	{mso-style-name:'Текст примечания Знак';
+	mso-style-link:'Текст примечания';
+	font-family:'Times New Roman',serif;}
+span.a0
+	{mso-style-name:'Тема примечания Знак';
+	mso-style-link:'Тема примечания';
+	font-family:'Times New Roman',serif;
+	font-weight:bold;}
+span.a1
+	{mso-style-name:'Текст выноски Знак';
+	mso-style-link:'Текст выноски';
+	font-family:'Tahoma',sans-serif;}
+span.a2
+	{mso-style-name:'Основной текст + Полужирный';
+	font-family:'Times New Roman',serif;
+	letter-spacing:0pt;
+	font-weight:bold;}
+p.a3, li.a3, div.a3
+	{mso-style-name:Тема;
+	margin-top:12.0pt;
+	margin-right:0cm;
+	margin-bottom:3.0pt;
+	margin-left:0cm;
+	page-break-after:avoid;
+	font-size:12.0pt;
+	font-family:'Arial',sans-serif;
+	font-weight:bold;
+	font-style:italic;}
+.MsoChpDefault
+	{font-size:10.0pt;
+	font-family:'Calibri',sans-serif;}
+@page WordSection1
+	{size:595.3pt 841.9pt;
+	margin:2.0cm 42.5pt 42.55pt 3.0cm;}
+div.WordSection1
+	{page:WordSection1;}
+-->
+</style>
+";
